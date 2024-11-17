bcd9ca5c77ace5538737b26ff8346ac0a974d5c4
shahjay489 <shahjay489@gmail.com>
2024-11-16 21:18:43 -0500

    4. java first program

---
+++ src/com/jayshah/chapter_1/_4/exampleEmployee/EmployeeDetails.java
@@ -0,0 +1,60 @@
+package com.jayshah.chapter_1._4.exampleEmployee;
+
+/**
+ *Company : NVIDIA, MicroSoft, Google, Meta, APPLE
+ *Client : USA (City)
+ *Version : JDK 17
+ *Authors Name : Jay Shah
+ *this class represents employee details
+ *which have its phone no, name, emp id etc
+ *and this class also contains many variables and methods
+ */
+public class EmployeeDetails {
+    /**
+     * this is employee name
+     */
+    public String emp_name="deepak";
+    /**
+     * this is employee id
+     */
+    public int emp_id=101;
+    /**
+     *This is no-argument constructor
+     */
+    public void EmpDetails()
+    {
+
+    }
+
+    /**
+     *This is argument constructor
+     */
+    public void EmpDetails(int a)
+    {
+
+    }
+
+    /**
+     *this method calculates the annual salary of the employee
+     */
+    public void calAnnualSal()
+    {
+
+    }
+
+    /**
+     *this method is m1
+     */
+    public void m1()
+    {
+
+    }
+
+    /**
+     *this method is m2
+     */
+    public void m2()
+    {
+
+    }
+}
