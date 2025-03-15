27412e47b59c350a9a8d814fae873bd96cc8f614
shahjay489 <shahjay489@gmail.com>
2025-03-15 11:51:54 -0400

    Chapter-7 : Part-1 : 7.1 Exception and Error Introduction

---
+++ src/com/jayshah/chapter_7_Exception_Handling/_1_Error_Exception_intro/Test1.java
@@ -0,0 +1,16 @@
+package com.jayshah.chapter_7_Exception_Handling._1_Error_Exception_intro;
+
+public class Test1 {
+    int rollno = 10;
+
+    public static void main(String[] args) {
+        int a = 10, b = 20;
+        int c = a + b;
+
+        String s1 = "abc", s2 = "xyz";
+        String ss = s1 + s2;
+
+        char c1 = 'a', c2 = 'b';
+        //char cc=c1+c2;
+    }
+}
---
+++ src/com/jayshah/chapter_7_Exception_Handling/_1_Error_Exception_intro/Test2.java
@@ -0,0 +1,15 @@
+package com.jayshah.chapter_7_Exception_Handling._1_Error_Exception_intro;
+
+public class Test2
+{
+    public static void main(String[] args)
+    {
+        System.out.println("1");
+        System.out.println("2");
+        System.out.println("3");
+        System.out.println(100/0);
+        System.out.println("4");
+        System.out.println("5");
+        System.out.println("6");
+    }
+}
