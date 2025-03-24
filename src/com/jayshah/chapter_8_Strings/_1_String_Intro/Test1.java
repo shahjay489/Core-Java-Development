f3ce48d501943a8e89d9a36e94d35aa0a67449be
shahjay489 <shahjay489@gmail.com>
2025-03-24 01:04:32 -0400

    Chapter-8 : Part-1 : 8.1 String Introduction in Java

---
+++ src/com/jayshah/chapter_8_Strings/_1_String_Intro/Test1.java
@@ -0,0 +1,11 @@
+package com.jayshah.chapter_8_Strings._1_String_Intro;
+
+public class Test1 {
+    public static void main(String[] args) {
+        String name = "Govardhan";
+        //name=name+" java";
+        //name=name.concat(" java");
+        name.concat(" java");
+        System.out.println(name);
+    }
+}
---
+++ src/com/jayshah/chapter_8_Strings/_1_String_Intro/Test2.java
@@ -0,0 +1,18 @@
+package com.jayshah.chapter_8_Strings._1_String_Intro;
+
+final class Test {
+    void greet() {
+        System.out.println("hi");
+    }
+}
+
+public class Test2 //extends Test
+{
+    void greet() {
+        System.out.println("hello");
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
