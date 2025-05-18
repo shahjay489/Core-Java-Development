bdf72f48f40b332e1679846c1129c492c511ae81
shahjay489 <shahjay489@gmail.com>
2025-05-18 19:24:03 -0400

    Chapter-14 : Part-1 : Data Structure & Arrays (Intro) in Java

---
+++ src/com/jayshah/Chapter_14_Arrays/_1/demo2/Profile.java
@@ -0,0 +1,28 @@
+package com.jayshah.Chapter_14_Arrays._1.demo2;
+
+public class Profile {
+
+    String name;
+    String dob;
+    String city;
+    String phone;
+    int age;
+
+    // using constructor
+    Profile(String name, String dob, int age, String city, String phone) {
+        this.name = name;
+        this.dob = dob;
+        this.age = age;
+        this.city = city;
+        this.phone = phone;
+    }
+
+    void welcome() {
+        System.out.println("Welcome " + name);
+        System.out.println("DOB: " + dob);
+        System.out.println("Age: " + age);
+        System.out.println("City: " + city);
+        System.out.println("Phone: " + phone);
+    }
+
+}
