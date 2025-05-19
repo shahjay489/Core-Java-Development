38ab6372a8d73b537490f8e30c760d43799c4b81
shahjay489 <shahjay489@gmail.com>
2025-05-18 20:52:13 -0400

    Chapter-14 : Part-2 : Single Dimensional Array in Java

---
+++ src/com/jayshah/Chapter_14_Arrays/_2/Test1.java
@@ -0,0 +1,27 @@
+package com.jayshah.Chapter_14_Arrays._2;
+
+public class Test1 {
+
+    // int rollno;  // just variable
+
+    // int[] rollno; // Array Declaration
+
+
+    public static void main(String[] args) {
+//        int[] a = new int[3];   // creation
+//        
+//        a[0]=10;
+//        a[1]=20;
+//        a[2]=30;
+
+        int[] a = {10, 20, 30};
+
+        System.out.println("Array Length : " + a.length);
+
+        System.out.println("---------------------------------------");
+
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
+    }
+}
